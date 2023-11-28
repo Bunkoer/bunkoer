0191af3344380ca9746ba6a86f03a8604533d972
t0xan <t0xan@gmail.com>
2023-11-28 13:09:39 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bunkoer',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # liste des dépendances
+    ],
+    # autres métadonnées : auteur, description, etc.
+)
+
