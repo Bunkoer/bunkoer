5b48db7588a9a6f45e3a573466580ca433be7249
T0xan <96002702+Touxan@users.noreply.github.com>
2023-11-30 12:23:23 +0100

    Black List

--- bunkoer/security.py
+++ bunkoer/security.py
@@ -18,7 +18,12 @@ def SecureFile(file_path, delete_src=False):
         if header == None:
             print("[ERROR] during the csv anonymization ")
             exit(1)
-        prompt = csv_task.prompt_csv(header)
+
+        if os.path.isfile("./blacklist"):
+            blacklist = utils.read_blacklist_line("./blacklist")
+        else:
+            print("f[ERROR] No black list file find")
+        prompt = csv_task.prompt_csv(header, blacklist)
 
         bad_clumn =  gpt.send_gpt_resquest(prompt, model_name, temperature, max_tokens=None)
         if bad_clumn == None:
